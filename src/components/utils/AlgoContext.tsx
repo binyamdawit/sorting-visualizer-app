a005ee4033c902d01d35693b0a5934f696f6b323
binyamdawit <78832827+binyamdawit@users.noreply.github.com>
2022-10-24 09:13:25 -0500

    first commit

---
+++ src/components/utils/AlgoContext.tsx
@@ -0,0 +1,80 @@
+import { createContext, useEffect, useState } from "react";
+import { Algo, ISettingsContext, Items, Settings } from "./AlgoContext.types";
+import { getHeapSortAnims } from "./heapSort";
+import { getInsertionSortAnims } from "./insertionSort";
+import { getMergeSortAnims } from "./mergeSort";
+import { animateDivs, animateMerge } from "./animate";
+import { getQuickSortAnims } from "./quickSort";
+
+const initVals: Settings = {
+  algoType: "merge sort",
+  arrayLen: 125,
+  delay: 5,
+};
+
+export const SettingsContext = createContext<ISettingsContext>({
+  settings: initVals,
+  sort: () => {},
+});
+
+export const ItemsContext = createContext<Items>({ items: [] });
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AlgoContext: React.FC<Props> = ({ children }) => {
+  const [settings, setSettings] = useState<Settings>(initVals);
+  const [items, setItems] = useState<number[]>([]);
+
+  useEffect(() => {
+    const ranNums = [];
+    for (let i = 0; i < settings.arrayLen; i++) {
+      ranNums.push(Math.floor(Math.random() * 540));
+    }
+    setItems(ranNums);
+  }, [settings.arrayLen]);
+
+  const sort = (algoType: Algo) => {
+    switch (algoType) {
+      case "insertion sort":
+        const { newArr, animArr } = getInsertionSortAnims(items);
+        animateDivs({ newArr, arr: animArr, setItems, settings });
+        break;
+      case "merge sort":
+        const aux: number[] = [];
+        const arr: number[][] = [];
+        const nums = [...items];
+        getMergeSortAnims(nums, aux, arr, 0, items.length - 1);
+        animateMerge({ newArr: nums, arr, setItems, settings });
+        break;
+      case "heap sort":
+        const { heapSorted, heapSortAnims } = getHeapSortAnims(items);
+        animateDivs({
+          newArr: heapSorted,
+          arr: heapSortAnims,
+          setItems,
+          settings,
+        });
+        break;
+      case "quick sort":
+        const { quickSorted, quickSortAnims } = getQuickSortAnims(items);
+        animateDivs({
+          newArr: quickSorted,
+          arr: quickSortAnims,
+          setItems,
+          settings,
+        });
+    }
+  };
+
+  return (
+    <ItemsContext.Provider value={{ items, setItems }}>
+      <SettingsContext.Provider value={{ sort, settings, setSettings }}>
+        {children}
+      </SettingsContext.Provider>
+    </ItemsContext.Provider>
+  );
+};
+
+export default AlgoContext;
